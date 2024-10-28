d398a69b3a8fc454454eaab4f22040b47a99a510
이재영 <ldc1000@naver.com>
2024-10-28 15:04:04 +0900

    Refator: calculate 정적메서드로 수정

--- Calculator.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Calculator.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -10,20 +10,20 @@ import Foundation
 struct Calculator {
     
     // Operator 열거형을 통해 연산자 입력
-    func calculate(_ operator: Self.Operator, firstNumber: Int, secondNumber: Int ) {
+    static func calculate(_ operator: Self.Operator, firstNumber: Int, secondNumber: Int) {
         
         switch `operator`{
         case .add:
-            AddOperation().calculate(firstNumber, secondNumber)
+            AddOperation.calculate(firstNumber, secondNumber)
             
         case .substract:
-            SubstractOperation().calculate(firstNumber, secondNumber)
+            SubstractOperation.calculate(firstNumber, secondNumber)
             
         case .multiply:
-            MultiplyOperation().calculate(firstNumber, secondNumber)
+            MultiplyOperation.calculate(firstNumber, secondNumber)
 
         case .divide:
-            DivideOperation().calculate(firstNumber,secondNumber)
+            DivideOperation.calculate(firstNumber,secondNumber)
         }
     }
     
@@ -31,26 +31,26 @@ struct Calculator {
     // 연산자 프로토콜
     // - calculate 메서드 필수 구현
     private protocol AbstractOperation {
-        func calculate(_ firstNumber: Int, _ secondNumber: Int)
+        static func calculate(_ firstNumber: Int, _ secondNumber: Int)
     }
     
     // 덧셈 연산자 구조체
     private struct AddOperation: AbstractOperation {
-        func calculate(_ firstNumber: Int,_ secondNumber: Int) {
+        static func calculate(_ firstNumber: Int,_ secondNumber: Int) {
             return print("\(firstNumber) + \(secondNumber) = \(firstNumber + secondNumber)")
         }
     }
     
     // 뺄셈 연산자 구조체
     private struct SubstractOperation: AbstractOperation {
-        func calculate(_ firstNumber: Int,_ secondNumber: Int) {
+        static func calculate(_ firstNumber: Int,_ secondNumber: Int) {
             print("\(firstNumber) - \(secondNumber) = \(firstNumber - secondNumber)")
         }
     }
     
     // 곱셈 연산자 구조체
     private struct MultiplyOperation: AbstractOperation {
-        func calculate(_ firstNumber: Int,_ secondNumber: Int) {
+        static func calculate(_ firstNumber: Int,_ secondNumber: Int) {
             print("\(firstNumber) * \(secondNumber) = \(firstNumber * secondNumber)")
         }
     }
@@ -58,7 +58,7 @@ struct Calculator {
     
     // 나눗셈 연산자 구조체
     private struct DivideOperation: AbstractOperation {
-        func calculate(_ firstNumber: Int,_ secondNumber: Int) {
+        static func calculate(_ firstNumber: Int,_ secondNumber: Int) {
             guard secondNumber != 0 else {
                 print("0으로는 나눌 수 없습니다.")
                 return
@@ -77,10 +77,9 @@ struct Calculator {
 }
 
 
-let calculator = Calculator()
 
-calculator.calculate(.add, firstNumber: 10, secondNumber: 3)
-calculator.calculate(.substract, firstNumber: 10, secondNumber: 3)
-calculator.calculate(.multiply, firstNumber: 10, secondNumber: 3)
-calculator.calculate(.divide, firstNumber: 10, secondNumber: 3)
-calculator.calculate(.divide, firstNumber: 10, secondNumber: 0)
+Calculator.calculate(.add, firstNumber: 10, secondNumber: 3)
+Calculator.calculate(.substract, firstNumber: 10, secondNumber: 3)
+Calculator.calculate(.multiply, firstNumber: 10, secondNumber: 3)
+Calculator.calculate(.divide, firstNumber: 10, secondNumber: 3)
+Calculator.calculate(.divide, firstNumber: 10, secondNumber: 0)
