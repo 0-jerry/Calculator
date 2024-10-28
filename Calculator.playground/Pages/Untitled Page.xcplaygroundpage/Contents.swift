676b43595c4da1ec4aa8ff8f39c4d475bf0ac706
이재영 <ldc1000@naver.com>
2024-10-28 18:13:21 +0900

    Fix&Chore: 예외처리 추가, 출력형식 수정
    - Int의 범위를 벗어나는 값에 대한 연산을 guard문을 통해 예외처리
    - 출력 형식 수정

--- Calculator.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Calculator.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -21,7 +21,7 @@ struct Calculator {
             
         case .multiply:
             MultiplyOperation.calculate(firstNumber, secondNumber)
-
+            
         case .divide:
             DivideOperation.calculate(firstNumber,secondNumber)
         }
@@ -37,36 +37,97 @@ struct Calculator {
     // 덧셈 연산자 구조체
     private struct AddOperation: AbstractOperation {
         static func calculate(_ firstNumber: Int,_ secondNumber: Int) {
-            return print("\(firstNumber) + \(secondNumber) = \(firstNumber + secondNumber)")
+            
+            let firstNumberStr = firstNumber >= 0 ? "\(firstNumber)" : "(\(firstNumber))"
+            let secondNumberStr = secondNumber >= 0 ? "\(secondNumber)" : "(\(secondNumber))"
+            
+            //합이 Int.max를 이하이며, Int.min이상
+            guard (firstNumber > 0 && Int.max - firstNumber >= secondNumber) ||
+                    (firstNumber < 0 && Int.min - firstNumber <= secondNumber) ||
+                    (firstNumber == 0) || (secondNumber == 0) else {
+                print("연삼 범위를 초과하였습니다.")
+                return
+            }
+            
+            print("\(firstNumberStr) ➕ \(secondNumberStr) 🟰 \(firstNumber + secondNumber)")
         }
     }
     
     // 뺄셈 연산자 구조체
     private struct SubstractOperation: AbstractOperation {
         static func calculate(_ firstNumber: Int,_ secondNumber: Int) {
-            print("\(firstNumber) - \(secondNumber) = \(firstNumber - secondNumber)")
+            
+            let firstNumberStr = firstNumber >= 0 ? "\(firstNumber)" : "(\(firstNumber))"
+            let secondNumberStr = secondNumber >= 0 ? "\(secondNumber)" : "(\(secondNumber))"
+            
+            //차가 Int.max 이하이며, Int.min이상
+            guard (firstNumber == 0) || (secondNumber == 0) ||
+                    (firstNumber > 0 && Int.max - firstNumber <= secondNumber) ||
+                    (firstNumber < 0 && firstNumber - Int.min >= secondNumber ) else {
+                print("연산 범위를 초과하였습니다.")
+                return
+            }
+            
+            print("\(firstNumberStr) ➖ \(secondNumberStr) 🟰 \(firstNumber - secondNumber)")
         }
     }
     
     // 곱셈 연산자 구조체
     private struct MultiplyOperation: AbstractOperation {
         static func calculate(_ firstNumber: Int,_ secondNumber: Int) {
-            print("\(firstNumber) * \(secondNumber) = \(firstNumber * secondNumber)")
+            
+            let firstNumberStr = firstNumber >= 0 ? "\(firstNumber)" : "(\(firstNumber))"
+            let secondNumberStr = secondNumber >= 0 ? "\(secondNumber)" : "(\(secondNumber))"
+            
+            guard firstNumber != Int.min else {
+                if [0,1].contains(secondNumber) {
+                    print("\(firstNumberStr) ✖️ \(secondNumberStr) 🟰 \(firstNumber * secondNumber)")
+                } else {
+                    print("연산 범위를 초과하였습니다.")
+                }
+                return
+            }
+            
+            guard secondNumber != Int.min else {
+                if [0,1].contains(firstNumber) {
+                    print("\(firstNumberStr) ✖️ \(secondNumberStr) 🟰 \(firstNumber * secondNumber)")
+                } else {
+                    print("연산 범위를 초과하였습니다.")
+                }
+                return
+            }
+            
+            let firstNumberAbs = abs(firstNumber)
+            let secondNumberAbs = abs(secondNumber)
+            
+            // 두 수의 절대값의 곱이 Int.max 이하 (단, Int의 나누기 연산으로 인해 오차 발생 가능)
+            guard [0,1].contains(firstNumberAbs) || [0,1].contains(secondNumberAbs) ||
+                    (firstNumberAbs <= Int.max / secondNumberAbs) else {
+                print("연산 범위를 초과하였습니다.")
+                return
+            }
+            
+            print("\(firstNumber) ✖️ \(secondNumber) 🟰 \(firstNumber * secondNumber)")
         }
     }
     
-    
+//    Int.max >= firstNumber * secondNumber
     // 나눗셈 연산자 구조체
     private struct DivideOperation: AbstractOperation {
         static func calculate(_ firstNumber: Int,_ secondNumber: Int) {
+            
             guard secondNumber != 0 else {
                 print("0으로는 나눌 수 없습니다.")
                 return
             }
-            print("\(firstNumber) / \(secondNumber) = \(firstNumber / secondNumber)∙∙∙\(firstNumber % secondNumber)")
+            
+            let firstNumberStr = firstNumber >= 0 ? "\(firstNumber)" : "(\(firstNumber))"
+            let secondNumberStr = secondNumber >= 0 ? "\(secondNumber)" : "(\(secondNumber))"
+            
+            print("\(firstNumberStr) ➗ \(secondNumberStr) 🟰 \(firstNumber / secondNumber)∙∙∙\(firstNumber % secondNumber)")
         }
     }
-
+    
     // 연산자 열거형
     enum Operator {
         case add
@@ -77,9 +138,13 @@ struct Calculator {
 }
 
 
-
-Calculator.calculate(.add, firstNumber: 10, secondNumber: 3)
 Calculator.calculate(.substract, firstNumber: 10, secondNumber: 3)
 Calculator.calculate(.multiply, firstNumber: 10, secondNumber: 3)
-Calculator.calculate(.divide, firstNumber: 10, secondNumber: 3)
+Calculator.calculate(.divide, firstNumber: 10, secondNumber: -3)
 Calculator.calculate(.divide, firstNumber: 10, secondNumber: 0)
+
+Calculator.calculate(.add, firstNumber: Int.max, secondNumber: 1)
+Calculator.calculate(.add, firstNumber: Int.min, secondNumber: -1)
+
+Calculator.calculate(.substract, firstNumber: Int.min, secondNumber: 1)
+Calculator.calculate(.substract, firstNumber: -1, secondNumber: Int.max)
