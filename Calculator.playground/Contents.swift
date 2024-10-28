223ad7fd162ff30ded0d23833bad2722442c387e
이재영 <ldc1000@naver.com>
2024-10-28 11:52:41 +0900

    Feat AddOperation
    - 덧셈 연산을 위한 구조체
    - calculate 메서드를 통해 연산값 반환

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -9,6 +9,11 @@ import Foundation
 
 struct Calculator {
     
+    private struct AddOpreration {
+        func calculate(_ firstNumber: Double,_ secondNumber: Double) -> Double {
+            return firstNumber + secondNumber
+        }
+    }
     enum Operator {
         case add
         case substract
